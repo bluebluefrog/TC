2822b780b570cfd324cd55bd7c42562d949c03e1
jingbin.wang <79555784+bluebluefrog@users.noreply.github.com>
2023-10-05 17:02:07 +1100

    new

---
+++ AssessmentTask2/src/SyntacticAnalyser.java
@@ -0,0 +1,56 @@
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class SyntacticAnalyser {
+
+	public static ParseTree parse(List<Token> tokens) throws SyntaxException {
+		//Turn the List of Tokens into a ParseTree.
+		return new ParseTree();
+	}
+
+}
+
+// The following class may be helpful.
+
+class Pair<A, B> {
+	private final A a;
+	private final B b;
+
+	public Pair(A a, B b) {
+		this.a = a;
+		this.b = b;
+	}
+
+	public A fst() {
+		return a;
+	}
+
+	public B snd() {
+		return b;
+	}
+
+	@Override
+	public int hashCode() {
+		return 3 * a.hashCode() + 7 * b.hashCode();
+	}
+
+	@Override
+	public String toString() {
+		return "{" + a + ", " + b + "}";
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if ((o instanceof Pair<?, ?>)) {
+			Pair<?, ?> other = (Pair<?, ?>) o;
+			return other.fst().equals(a) && other.snd().equals(b);
+		}
+
+		return false;
+	}
+
+}
