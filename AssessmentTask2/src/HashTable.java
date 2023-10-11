35f53bd7dcb4510b7ca84ee962af208e8ab028f4
kaigagi <kaigagipro@gmail.com>
2023-10-11 21:26:34 +1100

    bug fix

--- AssessmentTask2/src/HashTable.java
+++ AssessmentTask2/src/HashTable.java
@@ -3,7 +3,7 @@ public class HashTable<K, V> {
     public ArrayList<Pair<Integer, V>> table = new ArrayList<>();
 
     public void addPair(K key, V value){
-        table.add(new Pair<>(key.hashCode(), value));
+        table.add(new Pair<>(key.toString().hashCode(), value));
     }
 
     public void removePair(K key){
